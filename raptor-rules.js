5a14281327042a1745b89da7a8675089f8ecc5d9
PentruCaBlana <58446418+PentruCaBlana@users.noreply.github.com>
2023-03-31 13:40:42 +0100

    Add files via upload

---
+++ raptor-rules.js
@@ -0,0 +1,37 @@
+Hooks.on('ready', () => {
+	
+	//Weapon Properties
+	CONFIG.DND5E.weaponProperties['armorpiercing'] = 'Armor Piercing';
+	CONFIG.DND5E.weaponProperties['aquatic'] = 'Aquatic';
+	CONFIG.DND5E.weaponProperties['capture'] = 'Capture';
+	CONFIG.DND5E.weaponProperties['concussive'] = 'Concussive';
+	CONFIG.DND5E.weaponProperties['defensible'] = 'Defensible';
+	CONFIG.DND5E.weaponProperties['dry'] = 'Dry';
+	CONFIG.DND5E.weaponProperties['fist'] = 'Fist';
+	CONFIG.DND5E.weaponProperties['graze'] = 'Graze';
+	CONFIG.DND5E.weaponProperties['hooked'] = 'Hooked';
+	CONFIG.DND5E.weaponProperties['innacurate'] = 'Inaccurate';
+	CONFIG.DND5E.weaponProperties['long'] = 'Long';
+	CONFIG.DND5E.weaponProperties['mounted'] = 'Mounted';
+	CONFIG.DND5E.weaponProperties['misfire'] = 'Misfire';
+	CONFIG.DND5E.weaponProperties['nimble'] = 'Nimble';
+	CONFIG.DND5E.weaponProperties['parrying'] = 'Parrying';
+	CONFIG.DND5E.weaponProperties['pointblank'] = 'Point Blank';
+	CONFIG.DND5E.weaponProperties['rapid'] = 'Rapid';
+	CONFIG.DND5E.weaponProperties['razor'] = 'Razor';
+	CONFIG.DND5E.weaponProperties['reloading'] = 'Reloading';
+	CONFIG.DND5E.weaponProperties['savage'] = 'Savage';
+	CONFIG.DND5E.weaponProperties['scatter'] = 'Scatter';
+	CONFIG.DND5E.weaponProperties['shieldbreaker'] = 'Shield-Breaker';
+  	CONFIG.DND5E.weaponProperties['short'] = 'Short';
+	CONFIG.DND5E.weaponProperties['steady'] = 'Steady';
+	CONFIG.DND5E.weaponProperties['swift'] = 'Swift';
+	CONFIG.DND5E.weaponProperties['unmountable'] = 'Unmountable';
+	CONFIG.DND5E.weaponProperties['unwieldy'] = 'Unwieldy';
+	CONFIG.DND5E.weaponProperties['worn'] = 'Worn';
+	
+	//Spell Schools
+	
+	CONFIG.DND5E.spellSchools.chronomancy = "Chronomancy";
+	CONFIG.DND5E.spellSchools.hemomancy = "Hemomancy";
+});
